085f008772778a451f63c260c0b7c58b312bc502
benjaminj6 <benjamin.d.johnson@icloud.com>
2016-07-23 11:52:45 -0700

    added open lib api bc of google books api quota issue. open lib api requests working

--- js/app.js
+++ js/app.js
@@ -1,32 +1,92 @@
 //function that handles creating the html code to be added to the DOM
 
 //function that handles getting the recommendations from the TasteKid API.
-function getRecommendations(bookTitle) {
+function getSuggestions(bookTitle) {
 	var params = {
 		q: bookTitle,
 		type: "books",
 		info: 1,
+		limit: 10,
 		k: "233177-bookfind-G83JWAJK",
 		callback: "?"
-	}
+	};
 
 	$.ajax({
 		url: "https://www.tastekid.com/api/similar?",
 		data: params,
 		dataType: "jsonp",
-		type: "GET"
+		type: "GET",
 	})
-	.done(function(jsonp) {
-		console.log(jsonp);
+	.done(function(results) {
+		console.log(results);
+		var suggestionResults = results.Similar.Results; 
+
+		$.each(suggestionResults, function(index, item) {
+			console.log("Item #" + (index + 1) + ": " + item.Name);
+			getBookInfo(item.Name);
+			//Code to generate the html list items
+		});
 	})
-	.fail(function(jsonp) {
-		//code which displays the error message
-	})
+	.fail(function(jqXHR, error) {
+	//code which displays the error 
+	console.log(error);
+	});
 }
-//function that handles getting the info for each recommendation from the Google
+
+function createSuggestionHTML(book) {
+
+}
+
+// function that retrieves book data for populating the list items.
+function getBookInfo(bookTitle) {
+	var params = {
+		q: bookTitle,
+	} 
+
+	$.ajax({
+		url: "//openlibrary.org/search.json?",
+		data: params,
+		dataType: "json"
+	})
+	.done(function(results) {
+		console.log(results);	
+
+		$.each(results.docs, function(index, item) {
+			var thisTitle = item.title;
+			
+			if(thisTitle === bookTitle) {
+				console.log(thisTitle);
+				return false;
+			}
+		})
+		//code to be executed goes here
+	});
+}
+
+//function that handles getting the info for a single recommendation from the Google
 //Books API
+/*function getBookInfo(bookTitle) {
+	var param = {
+		q: "\"" + bookTitle + "\"",
+		key: "AIzaSyD6ur9ubG33m5ZcajPZVnS-ofqwg9wR4xs"
+	};
 
+	var googleURL = "https://www.googleapis.com/books/v1/volumes?";
 
+	$.ajax({
+		url: googleURL,
+		data: param,
+	})
+	.done(function(results) {
+		console.log(results);
+		return results;
+	})
+	.fail(function(jqXHR, error) {
+		console.log(error);
+	});
+}*/
+
+/*----------------------------------------------------------------------*/
 $(document).ready(function() {
 
 	// user submits the form with either button or enter
@@ -39,7 +99,20 @@ $(document).ready(function() {
 	//user clicks on 'See More'
 	$('.books-list').on('click', '.see-more', function() {
 		console.log('see-more clicked');
+		//code to generate additional info for clicked item
 	});
 
-	getRecommendations('war and peace');
-})
+/*---- TESTING GROUNDS ----*/	
+	getSuggestions('dead souls');
+	// getBookInfo('njals saga')
+
+
+
+
+
+
+});
+
+
+
+
