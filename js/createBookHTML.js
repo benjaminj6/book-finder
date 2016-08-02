1073616f91a8b475fa4cfd761b90855c0f524f8d
benjaminj6 <benjamin.d.johnson@icloud.com>
2016-08-02 16:00:15 -0700

    refactoring

--- js/createBookHTML.js
+++ js/createBookHTML.js
@@ -4,15 +4,15 @@ var validateInfo = require('./validateInfo');
 // TO DO : refactor when I find the time
 function createBookHTML(bookTitle) {
 
-  let $thisBookHTML = $('.template li').clone();
+  var $thisBookHTML = $('.template li').clone();
 
-  let author = bookTitle.authors[0];
-  let publishDate = bookTitle.publishedDate;
-  let infoLink = bookTitle.infoLink; 
-  let title = bookTitle.title;
-  let description = bookTitle.description;
-  let publisher =   validateInfo(bookTitle, 'publisher', bookTitle.publisher, 'Unknown Publisher');
-  let pageCount = validateInfo(bookTitle, 'pageCount', `${bookTitle.pageCount}pgs.`, 'Page Info Unavailable');
+  var author = bookTitle.authors[0];
+  var publishDate = bookTitle.publishedDate;
+  var infoLink = bookTitle.infoLink; 
+  var title = bookTitle.title;
+  var description = bookTitle.description;
+  var publisher =   validateInfo(bookTitle, 'publisher', bookTitle.publisher, 'Unknown Publisher');
+  var pageCount = validateInfo(bookTitle, 'pageCount', bookTitle.pageCount + 'pgs.', 'Page Info Unavailable');
 
   // Special test for image availability
   if (!bookTitle.hasOwnProperty('imageLinks')) {
@@ -31,9 +31,6 @@ function createBookHTML(bookTitle) {
 
   $('.books-list').append($thisBookHTML);
 }
-
-var createMessage = (element, message) => element = message;
-
 
 module.exports = createBookHTML;
 
